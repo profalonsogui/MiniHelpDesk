7723b8d0a4a71c96f6960937cd85a035367a0f05
Guilherme <alonsogui011@gmail.com>
2026-02-28 11:59:25 -0300

    add comments in ChamadosController

--- MiniHelpDesk/Controllers/ChamadosController.cs
+++ MiniHelpDesk/Controllers/ChamadosController.cs
@@ -2,10 +2,25 @@
 
 namespace MiniHelpDesk.Controllers
 {
+    // Controller responsável pelo módulo de Chamados
+    // Ele recebe requisições relacionadas a /Chamados
     public class ChamadosController : Controller
     {
+        // Action padrão do módulo
+        // Quando acessamos /Chamados, essa função é executada
         public IActionResult Index()
         {
+            // Retorna a View correspondente
+            // O ASP.NET procura automaticamente por:
+            // Views/Chamados/Index.cshtml
+            return View();
+        }
+
+        // Action adicional apenas para exemplo didático
+        // Será acessada por /Chamados/Sobre
+        public IActionResult Sobre()
+        {
+            // Também retorna uma View chamada Sobre.cshtml
             return View();
         }
     }
