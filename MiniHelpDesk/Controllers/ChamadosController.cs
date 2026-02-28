0c38d7748ea5d90646fcb567d9ba2d3357cf7d3a
Guilherme <alonsogui011@gmail.com>
2026-02-28 11:52:24 -0300

    criação do ChamadosController e a View

---
+++ MiniHelpDesk/Controllers/ChamadosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniHelpDesk.Controllers
+{
+    public class ChamadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
