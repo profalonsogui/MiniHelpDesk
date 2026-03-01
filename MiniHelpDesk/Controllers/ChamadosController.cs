20711c928ed2afb029e4df3040bf66e81f60a6ff
Guilherme <alonsogui011@gmail.com>
2026-02-28 22:28:06 -0300

    Aula 02 - Model Chamado e listagem

--- MiniHelpDesk/Controllers/ChamadosController.cs
+++ MiniHelpDesk/Controllers/ChamadosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MiniHelpDesk.Models;
 
 namespace MiniHelpDesk.Controllers
 {
@@ -10,10 +11,16 @@ namespace MiniHelpDesk.Controllers
         // Quando acessamos /Chamados, essa função é executada
         public IActionResult Index()
         {
+            // Lista simulado dadisos de chamados (em um cenário real, isso viria de um banco de dados)
+            var chamados = new List<Chamado>
+            {
+                  new Chamado { Id = 1, Titulo = "Problema de Login", Descricao = "Usuário não consegue fazer login." },
+                  new Chamado { Id = 2, Titulo = "Erro na Impressora", Descricao = "A impressora não está funcionando." },
+            };
             // Retorna a View correspondente
             // O ASP.NET procura automaticamente por:
             // Views/Chamados/Index.cshtml
-            return View();
+            return View(chamados);
         }
 
         // Action adicional apenas para exemplo didático
---
+++ MiniHelpDesk/Models/ChamadoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniHelpDesk.Models
+{
+    // Representa um chamado do sistema
+    public class Chamado
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
